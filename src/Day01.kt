306071df9121c9dd5de0994e1dbed027f104c019
achim.weste <habe@keine.de>
2024-12-01 12:30:16 +0100

    Day01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,52 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val left = mutableListOf<Int>()
+    val right = mutableListOf<Int>()
+
+    fun part1(): Int {
+        var sum = 0
+
+        for (i in 0..left.size - 1)
+            sum += abs(left.get(i) - right.get(i))
+
+        return sum
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(): Int {
+        var sum = 0
+        val searchedNumbers = mutableMapOf<Int, Int>()
+
+        for (l in left)
+            sum += searchedNumbers.getOrPut(l) {
+                right.count { it == l } * l
+            }
+
+        return sum
+    }
+
+    fun initListsLeftAndRight(input: List<String>) {
+        left.clear()
+        right.clear()
+
+        for (line in input) {
+            val leftAndRight = line.split(" +".toRegex())
+            left.add(leftAndRight.get(0).toInt())
+            right.add(leftAndRight.get(1).toInt())
+        }
+
+        right.sort()
+        left.sort()
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    initListsLeftAndRight(readInput("Day01_test"))
+    check(part1() == 11)
+    check(part2() == 31)
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    initListsLeftAndRight(readInput("Day01"))
+
+    part1().println()
+    part2().println()
 }
